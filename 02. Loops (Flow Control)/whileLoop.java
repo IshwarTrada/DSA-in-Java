154586ef7db943940a5a715eb6e6172f6ac4a81b
IshwarTrada <ishwartrada15@gmail.com>
2024-01-03 19:39:50 +0530

    while loop example

---
+++ 02. Loops (Flow Control)/whileLoop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/* whileLoop */
+public class whileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nSum of first N natural numbers");
+        System.out.print("Enter a range of number :");
+        int n = sc.nextInt();
+        int sum = 0, i = 1;
+        while (i <= n) {
+            sum += i;
+            i++;
+        }
+        System.out.println("Sum of first " + n + "natural numbers are " + sum);
+    }
+}
