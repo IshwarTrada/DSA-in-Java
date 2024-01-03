96fad321622dc35d4c5902ea3e812a9c6b21dd6b
IshwarTrada <ishwartrada15@gmail.com>
2024-01-03 19:26:47 +0530

    Leap Year

--- 01. Conditional Statements/Leap Year/LeapYear.java
+++ 01. Conditional Statements/Leap Year/LeapYear.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class LeapYear {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a year : ");
             int year = sc.nextInt();
             if (year % 4 == 0) {
                 if (year % 100 == 0) {
