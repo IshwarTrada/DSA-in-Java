f933a5413d468b82e18745b40b5965ce7d1835c3
IshwarTrada <ishwartrada15@gmail.com>
2024-01-03 18:53:39 +0530

    Change in data types

--- 01. Conditional Statements/Calculator/calc.java
+++ 01. Conditional Statements/Calculator/calc.java
@@ -3,32 +3,33 @@ import java.util.Scanner;
 /* Calculator using switch */
 public class calc {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("\nEnter first value : ");
-        int a = sc.nextInt();
-        System.out.print("Enter second value : ");
-        int b = sc.nextInt();
-        System.out.print("Enter Operator (+,-,*,/,%) : ");
-        char operator = sc.next().charAt(0);
-        switch (operator) {
-            case '+':
-                System.out.println(a+b);
-                break;
-            case '-':
-                System.out.println(a-b);
-                break;
-            case '*':
-                System.out.println(a*b);
-                break;
-            case '/':
-                System.out.println(a/b);
-                break;
-            case '%':
-                System.out.println(a%b);
-                break;
-        
-            default:
-                System.out.println("Calculator is not that much advanced which can perform " + operator + " operation.");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("\nEnter first value : ");
+            float a = sc.nextFloat();
+            System.out.print("Enter second value : ");
+            float b = sc.nextFloat();
+            System.out.print("Enter Operator (+,-,*,/,%) : ");
+            char operator = sc.next().charAt(0);
+            switch (operator) {
+                case '+':
+                    System.out.println(a+b);
+                    break;
+                case '-':
+                    System.out.println(a-b);
+                    break;
+                case '*':
+                    System.out.println(a*b);
+                    break;
+                case '/':
+                    System.out.println(a/b);
+                    break;
+                case '%':
+                    System.out.println(a%b);
+                    break;
+            
+                default:
+                    System.out.println("Calculator is not that much advanced which can perform " + operator + " operation.");
+            }
         }
         System.out.println();
     }   
