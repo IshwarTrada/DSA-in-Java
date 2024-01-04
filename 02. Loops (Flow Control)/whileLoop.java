1bdd8ca5fb3dd43095dfbaaf1c7c06e359907031
IshwarTrada <ishwartrada15@gmail.com>
2024-01-04 22:59:24 +0530

    Loops in java

--- 02. Loops (Flow Control)/whileLoop.java
+++ 02. Loops (Flow Control)/whileLoop.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 /* whileLoop */
 public class whileLoop {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("\nSum of first N natural numbers");
-        System.out.print("Enter a range of number :");
-        int n = sc.nextInt();
-        int sum = 0, i = 1;
-        while (i <= n) {
-            sum += i;
-            i++;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("\nSum of first N natural numbers");
+            System.out.print("Enter a range of number :");
+            int n = sc.nextInt();
+            int sum = 0, i = 1;
+            while (i <= n) {
+                sum += i;
+                i++;
+            }
+            System.out.println("Sum of first " + n + "natural numbers are " + sum);
         }
-        System.out.println("Sum of first " + n + "natural numbers are " + sum);
     }
 }
