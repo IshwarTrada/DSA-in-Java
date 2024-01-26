ca34fc709ee304300de5c42f2870ae694562271b
IshwarTrada <ishwartrada15@gmail.com>
2024-01-26 14:42:20 +0530

    Operations - Get ith bit value(0/1) from given decimal number

---
+++ 09. Bit Manipulation/getIthbit.java
@@ -0,0 +1,24 @@
+// Operations
+// Get ith bit value
+
+import java.util.*;
+
+public class getIthbit {
+    public static void oddOrEven(int n, int i) {
+        if ((n & (1 << i)) == 0) {
+            System.out.println("0 bit on " + i + " position in number " + n);
+        } else {
+            System.out.println("1 bit on " + i + " position in number " + n);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        System.out.print("Enter bit position to know the value of that bit : ");
+        int i = sc.nextInt();
+        oddOrEven(n, i);
+    }
+}
