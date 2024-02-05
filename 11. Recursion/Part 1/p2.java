09cf1d1d3f47fd06fa9c9262d261e52e4e651f51
IshwarTrada <ishwartrada15@gmail.com>
2024-02-05 17:20:07 +0530

    Print number from 1 to n using Recursion

---
+++ 11. Recursion/Part 1/p2.java
@@ -0,0 +1,30 @@
+// Print number from 1 to n using Recursion
+
+public class p2 {
+    // Approach 1
+    // public static void printInc(int n, int number) {
+    //      if (number == n) {
+    //          System.out.print(n);
+    //          return;
+    //      }
+    //      System.out.print(number + " ");
+    //      printInc(n, number + 1);
+    // }
+    // Approach 2
+    public static void printInc(int n) {
+        if (n==1) {
+            System.out.print("1 ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n + " ");
+    }
+
+    public static void main(String[] args) {
+        int n = 10, number = 1;
+        // For approach 1
+        // printInc(n, number);
+        // For approach 2
+        printInc(n);
+    }
+}
