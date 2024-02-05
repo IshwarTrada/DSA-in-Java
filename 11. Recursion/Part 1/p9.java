2277de5f5dae30f3e71ef63b3cc5425e73851412
IshwarTrada <ishwartrada15@gmail.com>
2024-02-05 19:49:48 +0530

    Print x^n using recursion

---
+++ 11. Recursion/Part 1/p9.java
@@ -0,0 +1,14 @@
+// Problem 9 : Print x^n
+
+public class p9 {
+    public static int power(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return (x * power(x, n - 1));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(power(2, 10));
+    }
+}
