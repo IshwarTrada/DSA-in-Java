b1eac9fb8f73b563ba786b6cd7536a4a312e985b
IshwarTrada <ishwartrada15@gmail.com>
2024-02-07 17:32:44 +0530

    Quick Sort

---
+++ 12. Divide & Conquer/QuickSort.java
@@ -0,0 +1,52 @@
+// Quick Sort
+// Time Complexity : For average case -> O(n*logn)
+//                   For worst case -> O(n^2)
+// Space Complexity : O(1)
+
+public class QuickSort {
+    // Print the array
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void quickSort(int arr[], int si, int ei) {
+        // Base case
+        if (si >= ei) {
+            return;
+        }
+
+        int pIdx = partition(arr, si, ei); // give a pivot index
+        quickSort(arr, si, pIdx - 1); // left
+        quickSort(arr, pIdx + 1, ei); // right
+    }
+
+    public static int partition(int arr[], int si, int ei) {
+        // pivot = last element
+        int pivot = arr[ei];
+        int i = si - 1; // In our case ,It is -1 in the beginning
+
+        for (int j = si; j < ei; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        // swap
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 9, 8, 2, 5 };
+        quickSort(arr, 0, arr.length - 1);
+        printArr(arr);
+    }
+}
