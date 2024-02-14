9665779076bb9c77644dd67ab3fe4dc2c9111799
IshwarTrada <ishwartrada15@gmail.com>
2024-02-14 09:40:14 +0530

    Find Subsets of a string using backtracking

---
+++ 13. Backtracking/p2.java
@@ -0,0 +1,27 @@
+// Find Subsets of a string
+// Time Complexity : O(n*(2^n))
+// Space Complexity : O(n)
+
+public class p2 {
+    public static void findSubsets(String str, String ans, int i) {
+        // Base case
+        if (i == str.length()) {
+            if (ans.length() == 0) {
+                System.out.println("null");
+            } else {
+                System.out.println(ans);
+            }
+            return;
+        }
+
+        // Yes choice
+        findSubsets(str, ans + str.charAt(i), i + 1);
+        // No choice
+        findSubsets(str, ans, i + 1);
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        findSubsets(str, "", 0);
+    }
+}
