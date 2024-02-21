94953305c89b14588826f0ba481e44669a754bf7
IshwarTrada <ishwartrada15@gmail.com>
2024-02-21 14:36:23 +0530

    Linked List - Remove element from 1st and last position

---
+++ 15. Linked List/Part1/LinkedListRemove.java
@@ -0,0 +1,105 @@
+// Linked Lists
+// Remove -> First position
+//        -> Last position
+
+public class LinkedListRemove {
+    // remove first node
+    public int removeFirst() {
+        if (size == 0) {
+            System.out.println("Linked list is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--; 
+        return val;
+    }
+
+    // remove last node
+    public int removeLast() {
+        if (size == 0) {
+            System.out.println("Linked list is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        // prev : i = size - 2
+        Node prev = head;
+        for (int i = 0; i < size-2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data; // tail.data
+        prev.next = null;
+        tail = prev;
+        size--; 
+        return val;
+    }
+
+    // Main Method
+    public static void main(String[] args) {
+        LinkedListRemove ll = new LinkedListRemove();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.addLast(6);
+        ll.print(); // 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
+
+        System.out.println(ll.removeFirst());
+        ll.print(); // 2 -> 9 -> 3 -> 4 -> null
+        System.out.println(ll.removeLast());
+        ll.print(); // 2 -> 9 -> 3 -> null
+    }
+
+    // Print Linked List - Time Complexity - O(n)
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    // Creation of Node with the help of class
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size; // by default java give value as 0, so we don't have to declare it
+
+    // add Last - Time Complexity --> O(1)
+    public void addLast(int data) {
+        // Step - 1 Create new node
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        // Step - 2 tail next = newNode
+        tail.next = newNode;
+
+        // Step - 3 tail = newNode
+        tail = newNode;
+    }
+}
