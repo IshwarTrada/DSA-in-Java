09108462031639ea6e6afb6423cdd56776912a7d
IshwarTrada <ishwartrada15@gmail.com>
2024-02-22 19:07:02 +0530

    Detecting cycle in LL (Floyd's Cycle Finding Algorithm)

---
+++ 15. Linked List/Part2/p1.java
@@ -0,0 +1,44 @@
+// Floyd's Cycle Finding algorithm
+// Detecing cycle in Linked List
+
+public class p1 {
+    public static boolean isCycle() {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next; // +1
+            fast = fast.next.next; // +2
+
+            if (slow == fast) {
+                return true; // cycle exists
+            }
+        }
+        return false; // cycle does not exist
+    }
+
+    public static void main(String[] args) {
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = head; // 1->2->3->1
+
+        System.out.println(isCycle());
+    }
+
+    // Creation of Node with the help of class
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size; // by default java give value as 0, so we don't have to declare it
+}
