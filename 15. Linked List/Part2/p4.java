2d2756450497b48c9f4a43308fc62a0d9e856018
IshwarTrada <ishwartrada15@gmail.com>
2024-03-31 20:57:20 +0530

    Merge  Sort On Linked List

---
+++ 15. Linked List/Part2/p4.java
@@ -0,0 +1,118 @@
+// Merge Sort On Linked List
+// Time Complexity : O(nlogn)
+
+public class p4 {
+
+    private Node getMid(Node head) {
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow; // mid node
+    }
+
+    private Node merge(Node head1, Node head2) {
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+
+        while (head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+    }
+
+    public Node mergeSort(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // Find mid
+        Node mid = getMid(head);
+
+        // Left and right MS
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+
+        // merge
+        return merge(newLeft, newRight);
+    }
+
+    public static void main(String[] args) {
+        p4 ll = new p4();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        // 5 -> 4 -> 3 -> 2 -> 1
+        ll.print();
+        ll.head = ll.mergeSort(ll.head);
+        ll.print();
+    }
+
+    // Creation of Node with the help of class
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size; // by default java give value as 0, so we don't have to declare it
+    
+    public void addFirst(int data) {
+        // Step - 1 Create new node
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        // Step - 2 newNode next = head
+        newNode.next = head;
+
+        // Step - 3 newNode head = newNode
+        head = newNode;
+    }
+
+   
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
