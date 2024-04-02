d7527068f6994ea9873dfb9b6c620d34ba06e38e
IshwarTrada <ishwartrada15@gmail.com>
2024-04-02 15:31:26 +0530

    Stack using Collections Framework

---
+++ 16. Stacks/Part 1/p3.java
@@ -0,0 +1,17 @@
+// Stack using Collections Framework
+import java.util.*;
+
+public class p3 {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+    
+}
