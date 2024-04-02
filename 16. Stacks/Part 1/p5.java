3864e90c7b426f4e368378a6b301faaf480d7887
IshwarTrada <ishwartrada15@gmail.com>
2024-04-02 15:52:57 +0530

    Reverse a string using Stack

---
+++ 16. Stacks/Part 1/p5.java
@@ -0,0 +1,26 @@
+// Reverse a string using Stack
+
+import java.util.*;
+
+public class p5 {
+
+    public static String reverseString(String str){
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while (idx < str.length()) {
+            s.push(str.charAt(idx++));
+        }
+
+        StringBuilder result = new StringBuilder("");
+        while (!s.isEmpty()) {
+            result.append(s.pop());
+        }
+        return result.toString();
+    }
+    
+    public static void main(String[] args) {
+        String str = "abc";
+        String result = reverseString(str);
+        System.out.println(result);
+    }
+}
