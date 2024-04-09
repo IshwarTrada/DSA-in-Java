d1e8038f8c15ca94c3f03df5924829288953aee3
IshwarTrada <ishwartrada15@gmail.com>
2024-04-09 20:29:36 +0530

    Queue and it's Operations using Collections Framework

---
+++ 17. Queue/p4.java
@@ -0,0 +1,19 @@
+// Queue using Collections Framework
+
+import java.util.*;
+
+public class p4 {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        // or
+        // Queue<Integer> q = new ArrayDeque<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
